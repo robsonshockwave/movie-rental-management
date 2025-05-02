33f16b9cc3cae9f931eedbd957c5727c416dbb1f
robson arruda <robsonshockwavegithub@gmail.com>
2025-05-02 12:30:35 -0300

    fix: ajuste na tipagem do hire repository

--- src/domain/repositories/IHireRepository.ts
+++ src/domain/repositories/IHireRepository.ts
@@ -2,12 +2,14 @@ import { IClient } from '../entities/Client';
 import { Hire, IHire } from '../entities/Hire';
 import { IMovie, Movie } from '../entities/Movie';
 
+interface IHireWithClientAndMovie extends IHire {
+  client: IClient;
+  movie: IMovie;
+}
+
 export interface IHireRepository {
   create(hire: Hire): Promise<IHire>;
   thisMovieHiredByClient(client_id: string, movie_id: string): Promise<boolean>;
-  getHireWithClientAndMovieById(id: string): Promise<{
-    hire: IHire;
-    client: IClient;
-    movie: IMovie;
-  }>;
+  getHireWithClientAndMovieById(id: string): Promise<IHireWithClientAndMovie>;
+  getPendingHires(): Promise<IHireWithClientAndMovie[]>;
 }
