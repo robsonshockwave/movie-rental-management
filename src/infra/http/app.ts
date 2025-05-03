52a7f7a93212bf2f4723cd6ed0b7fbd6673ed2f8
robson arruda <robsonshockwavegithub@gmail.com>
2025-05-03 16:18:39 -0300

    feat: iniciando a config de rota e adicionando o middlerawe de captura de erros

--- src/infra/http/app.ts
+++ src/infra/http/app.ts
@@ -1,7 +1,34 @@
-import express from 'express';
+import express, {
+  ErrorRequestHandler,
+  NextFunction,
+  Request,
+  Response,
+} from 'express';
+import { routes } from './routes';
+import { ZodError } from 'zod';
 
 const app = express();
 
 app.use(express.json());
+
+app.use(routes);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction): any => {
+  if (err instanceof ZodError) {
+    return res.status(400).json({
+      message: 'Erro de validação',
+      errors: err,
+    });
+  }
+
+  if (process.env.NODE_ENV !== 'production') {
+    console.error(err);
+  }
+
+  return res.status(500).json({
+    error: 'error',
+    message: 'Internal server error',
+  });
+});
 
 export { app };
