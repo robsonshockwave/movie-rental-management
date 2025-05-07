41c28d397c587717d3e63706e14586f938ec90fb
robson arruda <robsonshockwavegithub@gmail.com>
2025-05-07 20:37:45 -0300

    feat: adicionando bullboard para acompanhar as filas

--- src/infra/http/app.ts
+++ src/infra/http/app.ts
@@ -5,15 +5,30 @@ import { routes } from './routes';
 import { ZodError } from 'zod';
 import typeormServer from '../database/typeorm/setup';
 
+import { createBullBoard } from '@bull-board/api';
+import { BullAdapter } from '@bull-board/api/bullAdapter';
+import { ExpressAdapter } from '@bull-board/express';
+import { sendMailQueue } from './queue';
+
 const app = express();
 
 app.use(express.json());
 
 app.use(routes);
 
+// QUEUE MONITOR
+const serverAdapter = new ExpressAdapter();
+serverAdapter.setBasePath('/queues');
+createBullBoard({
+  queues: [new BullAdapter(sendMailQueue)],
+  serverAdapter,
+});
+
 typeormServer
   .initialize()
   .then(() => {
+    app.use('/queues', serverAdapter.getRouter());
+
     app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
       if (err instanceof ZodError) {
         // console.log(err.flatten().fieldErrors);
